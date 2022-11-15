f74a52afe94856f1eeb784632e1afa4cefdd3a61
SeyedMohammadAli Noori <13noori79@gmail.com>
2022-11-15 15:57:50 +0330

    Add 001.py

---
+++ 011.py
@@ -0,0 +1,18 @@
+x = 5
+y = 10
+z = 5
+w = 15
+
+# Boolean values: True, False
+
+print('x == y', x == y)
+print('x == z', x == z)
+
+print('y < w', y < w)
+print('y > w', y > w)
+
+print('y <= w', y <= w)
+print('y >= w', y >= w)
+
+print('x <= z', x <= z)
+print('x >= z', x >= z)
