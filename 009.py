6dd939b1851ad6d451971c05cc2e5c253bd69d51
SeyedMohammadAli Noori <13noori79@gmail.com>
2022-11-15 15:43:29 +0330

    Add 009.py

---
+++ 009.py
@@ -0,0 +1,14 @@
+x = 5
+print('x:', x)
+
+n = x * 2
+print('n:', n)
+
+m = 3 * 5 + 2
+print('m:', m)
+
+my_name = 'Ali'
+print(my_name)
+
+user_name = input('Enter your name: ')
+print('Hello', user_name)
