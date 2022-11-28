169be16648982e68f844fdbb95dc1070dd5fad0b
SeyedMohammadAli Noori <13noori79@gmail.com>
2022-11-28 17:32:22 +0330

    Add 013 & 014

---
+++ 013.py
@@ -0,0 +1,9 @@
+a = int(input('Enter number a: '))
+b = int(input('Enter number b: '))
+result = a + b
+print('a + b = ', result)
+
+a = float(input('Enter number a: '))
+b = float(input('Enter number b: '))
+result = a + b
+print(result)
---
+++ 014 - solution.py
@@ -0,0 +1,22 @@
+a = float(input('Enter number a: '))
+b = float(input('Enter number b: '))
+
+print('a + b = ', a + b)
+print('a - b = ', a - b)
+print('a * b = ', a * b)
+print('a / b = ', a / b)
+print('a ** b = ', a ** b)
+print('a // b = ', a // b)
+print('a % b = ', a % b)
+
+
+a = int(input('Enter number a: '))
+b = int(input('Enter number b: '))
+
+print('a + b = ', a + b)
+print('a - b = ', a - b)
+print('a * b = ', a * b)
+print('a / b = ', a / b)
+print('a ** b = ', a ** b)
+print('a // b = ', a // b)
+print('a % b = ', a % b)
